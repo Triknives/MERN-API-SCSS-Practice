48f51cdb4ade1a98cbf7c7fd2420e587e7bb8f84
Triknives <50305423+Triknives@users.noreply.github.com>
2020-05-11 22:53:15 -0700

    begin building forms, connecting back end to client side

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,10 +1,61 @@
-import React from 'react'
+import React, {Fragment, useState} from 'react'
+import { Link, Redirect } from 'react-router-dom';
 
-export const Login = () => {
+ const Login = () => {
+    const [formData, setFormData] = useState({
+        name:' ',
+        email:' ',
+        password:' ',
+        password2: ' '
+    });
+
+    const {email, password, password2} = formData;
+
+    const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+    const onSubmit = async e => {
+        e.preventDefault();
+        if(password !== password2){
+            console.log('Passwords do not match');  
+        }else {
+            console.log('SUCCESS')
+        }
+    };
+
     return (
-        <div>
-            Login
+<Fragment>
+      <h1 className="large text-primary">Sign In</h1>
+      <p className="lead">
+        <i className="fas fa-user" /> Sign Into Your Account
+      </p>
+      <form className="form" onSubmit={onSubmit}>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={onChange}
+            required
+          />
         </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={password}
+            onChange={onChange}
+            minLength="6"
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Login" />
+      </form>
+      <p className="my-1">
+        Don't have an account? <Link to="/register">Sign Up</Link>
+      </p>
+    </Fragment>
     )
 }
 export default Login;
