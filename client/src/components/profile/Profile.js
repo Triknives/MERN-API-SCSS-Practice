4abbb00513caedf30fc1955fe88a3d81bab365c4
Triknives <50305423+Triknives@users.noreply.github.com>
2020-05-16 15:53:02 -0700

    update to profile view. Begin adding edit profile fucntionality after viewing profile

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,6 +1,7 @@
 import React, {Fragment, useEffect} from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux';
+import {Link} from 'react-router-dom';
 import Spinner from '../layout/Spinner';
 import { getProfileById}  from '../../actions/profile';
 
@@ -14,8 +15,29 @@ const Profile = ({
       getProfileById(match.params.id);
     }, [getProfileById]);
 
-    return<div>profile</div>;
-}
+    return (
+      <Fragment>
+        {profile === null || loading ? (
+          <Spinner />
+        ) : (
+          <Fragment>
+            <Link to='/profiles' className='btn btn-light'>
+              Back To Profiles
+            </Link>
+            {auth.isAuthenticated &&
+              auth.loading === false &&
+              auth.user._id === profile.user._id && (
+                <Link to='/edit-profile' className='btn btn-dark'>
+                  Edit Profile
+                </Link>
+              )}
+           
+          </Fragment>
+        )}
+      </Fragment>
+    );
+  };
+  
 
 Profile.propTypes = {
     getProfileById: PropTypes.func.isRequired,
