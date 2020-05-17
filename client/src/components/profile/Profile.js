bbf912dfef122ea9eb1bdee9ed31c8269a90303c
Triknives <50305423+Triknives@users.noreply.github.com>
2020-05-16 21:27:09 -0700

    update profile to display Bio and Map skills.

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -5,6 +5,7 @@ import {Link} from 'react-router-dom';
 import Spinner from '../layout/Spinner';
 import { getProfileById}  from '../../actions/profile';
 import ProfileTop from './ProfileTop';
+import ProfileAbout from './ProfileAbout';
 
 const Profile = ({
     getProfileById,
@@ -34,6 +35,8 @@ const Profile = ({
               )}
            <div class="profile-grid my-1">
              <ProfileTop profile={profile} />
+             <ProfileAbout profile={profile} />
+
            </div>
           </Fragment>
         )}
